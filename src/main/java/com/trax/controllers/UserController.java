d6ef48a2c5557b89377d0490ed457f1a3f3be1cc
Tony Winters <tony.winters@m33integrated.com>
2013-10-13 20:19:49 -0400

    More Dependencies

--- src/main/java/com/trax/controllers/UserController.java
+++ src/main/java/com/trax/controllers/UserController.java
@@ -7,7 +7,6 @@ import com.trax.services.owner.OwnerService;
 import com.trax.services.role.RoleService;
 import com.trax.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
